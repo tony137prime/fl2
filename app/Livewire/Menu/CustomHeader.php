d10eb4d4d2f51d33edc06b6043276f273a14eb9a
tony137prime <nemesisprime137@hotmail.com>
2025-05-28 18:38:21 +0200

    flux is working I just needed to ad @vite for css and also for js

---
+++ app/Livewire/Menu/CustomHeader.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Menu;
+
+use Livewire\Component;
+
+class CustomHeader extends Component
+{
+    public function render()
+    {
+        return view('livewire.menu.custom-header');
+    }
+}
